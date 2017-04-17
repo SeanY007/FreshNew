fe7808fcbc2470e3f3fb3e386319307eb7464763
Sean <zhipeng1102@msn.cn>
2017-04-17 19:02:50 +0800

    Chapter5 MoreAboutVariables

---
+++ Chapter5 MoreAboutVariables/Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Robert A", "Mike B", "Jeremy C" };
+            //for循环遍历
+            //int i;
+            //Console.WriteLine("Here are {0} of my friends:", friendNames.Length);//获取数组大小
+            //for (i = 0; i < friendNames.Length; i++)
+            //{
+            //    Console.WriteLine(friendNames[i]);
+            //}
+            //Console.ReadKey();
+
+            //foreach循环
+            //Console.WriteLine("Here are {0} of my friends:", friendNames.Length);
+            //foreach (string friendName in friendNames)
+            //{
+            //    Console.WriteLine(friendName); ;
+            //}
+            //Console.ReadKey();
+
+            string myString = "This is a test.";
+            char[] seperator = { ' ' };
+            string[] myWords;
+            myWords = myString.Split(seperator);
+            foreach(string word in myWords){
+                Console.WriteLine("{0}",word);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter5 MoreAboutVariables/TypeConversion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////checked/unchecked用法
+            //byte destinationVar;
+            ////short sourceVar = 7;
+            //short sourceVar = 281;//溢出，源数据最左一位丢失 281----->100011001----->000011001---->25
+            //destinationVar = (checked((byte)sourceVar));//显示转换 checked进行溢出检查
+            //Console.WriteLine("sourceVar val:{0}",sourceVar);
+            //Console.WriteLine("destination val:{0}",destinationVar);
+            //Console.ReadKey();
+
+            //Convert命令
+            short shortResult, shortVal = 4;
+            int integerVal = 67;
+            long longResult;
+            float floatVal = 10.5f;
+            double doubleResult, doubleVal = 99.999;
+            string stringResult, stringVal = "17";
+            bool boolVal = true;
+
+            Console.WriteLine("Variable Conversion Example\n");
+            doubleResult = floatVal * shortVal;
+            Console.WriteLine("Implicit,->double:{0}*{1}->{2}", floatVal, shortVal, doubleResult);
+
+            shortResult = (short)floatVal;
+            Console.WriteLine("Explicit,->short:{0}->{1}",floatVal,shortVal);
+
+            stringResult = Convert.ToString(boolVal) + Convert.ToString(doubleVal);
+            Console.WriteLine("Explicit,->String:\"{0}\"+\"{1}\"->{2}",boolVal,doubleVal,stringResult);
+
+            longResult = integerVal + Convert.ToInt64(stringVal);
+            Console.WriteLine("Mixed,->long:{0}+{1}->{2}",integerVal,stringVal,longResult);
+            Console.ReadKey();
+        }
+    }
+}
